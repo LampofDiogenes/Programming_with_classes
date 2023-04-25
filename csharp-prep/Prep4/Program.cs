e78af622501bf95c355e5e43e36190f535e85ad3
Kaleb Bradford <wannabdiogenes@gmail.com>
2023-04-24 20:17:30 -0600

    Added comment for readability

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -12,21 +12,28 @@ class Program
 
         do
         {
+        // Getting input from User
         Console.Write("Please give us a number. (enter 0 to end the program): ");
         NewUserNumber = int.Parse(Console.ReadLine());
 
+        // Adding the input to a list
         numbers.Add(NewUserNumber);
+        
+        // looping through until the user enters 0
         }while (NewUserNumber != 0);
 
     int SumNumbers = 0;
 
+    // Calculating the Sum
     foreach (int UserNumber in numbers)
     {
         SumNumbers += UserNumber;
     }
 
+    // Displaying the Sum
     Console.WriteLine($"The Sum is: {SumNumbers}");
 
+    // Calculating the average
     int iterations = 0;
     foreach (int UserNumber in numbers)
     {
@@ -35,8 +42,10 @@ class Program
 
     int AverageNumbers = (SumNumbers / iterations);
 
+    // Displaying the Average
     Console.WriteLine($"The Average is {AverageNumbers}");
 
+    // Calculating the highest number that was entered
     int HighestNumber = 0;
     foreach (int UserNumber in numbers)
     {
@@ -44,7 +53,7 @@ class Program
             HighestNumber = UserNumber;
         }
     }
-
+    // Showing the highest number to the user
     Console.WriteLine($"The Highest Number you entered is: {HighestNumber}");
     }
 }
