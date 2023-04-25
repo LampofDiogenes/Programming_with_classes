30df277662500cc95a6f1f128a1d744a8c1688a8
Kaleb Bradford <wannabdiogenes@gmail.com>
2023-04-24 20:25:11 -0600

    finished the program which asks for the user's name, and their favorite number

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Console.WriteLine("Hello! Welcome to the Program!");
+
+        Console.Write("What is your name? ");
+        string UserName = Console.ReadLine();
+
+        Console.Write("What is you favorite Number? ");
+        int FavoriteNumber = int.Parse(Console.ReadLine());
+
+        int FavoriteNumberSquared = (FavoriteNumber * FavoriteNumber);
+
+        Console.Write($"Hello {UserName}, your favorite number squared is {FavoriteNumberSquared}.");
+
     }
 }
