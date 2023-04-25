db160f552aa3af20b81caf7cedfcb1ac586ecf68
Kaleb Bradford <wannabdiogenes@gmail.com>
2023-04-24 20:12:06 -0600

    Finished the program that calculates the sum, average, and highest int of entered numbers

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+
+    List<int> numbers;
+    numbers = new List<int>();
+    int NewUserNumber = 0;
+
+        do
+        {
+        Console.Write("Please give us a number. (enter 0 to end the program): ");
+        NewUserNumber = int.Parse(Console.ReadLine());
+
+        numbers.Add(NewUserNumber);
+        }while (NewUserNumber != 0);
+
+    int SumNumbers = 0;
+
+    foreach (int UserNumber in numbers)
+    {
+        SumNumbers += UserNumber;
+    }
+
+    Console.WriteLine($"The Sum is: {SumNumbers}");
+
+    int iterations = 0;
+    foreach (int UserNumber in numbers)
+    {
+        iterations++;
+    }
+
+    int AverageNumbers = (SumNumbers / iterations);
+
+    Console.WriteLine($"The Average is {AverageNumbers}");
+
+    int HighestNumber = 0;
+    foreach (int UserNumber in numbers)
+    {
+        if (UserNumber > HighestNumber){
+            HighestNumber = UserNumber;
+        }
+    }
+
+    Console.WriteLine($"The Highest Number you entered is: {HighestNumber}");
     }
 }
