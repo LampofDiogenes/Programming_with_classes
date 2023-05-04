6c93cab9abde7c4f41836a95bdef076238a5bdd0
Kaleb Bradford <wannabdiogenes@gmail.com>
2023-05-03 23:35:50 -0600

    added unusable code

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Console.WriteLine("Hello Learning World!");
     }
+}
+
+public class Job
+{
+
+public static void DisplayJob(string[] jobtitle, string[] company, int[] StartYear, int[] EndYear)
+    {
+        Console.WriteLine($"{jobtitle} ({company} {StartYear} {EndYear})");
+    }
+}
+
+public class Resume
+{
+
+
+    public list<Job> _jobs = new list<Job>();
+    public static void DisplayResume(string[] name, list<>)
+    {
+
+    }
+
 }
