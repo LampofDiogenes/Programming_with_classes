6f3d5d924f9b0b64d76d6b351703029487734740
Kaleb Bradford <wannabdiogenes@gmail.com>
2023-05-17 16:43:24 -0600

    finished 1st draft of fractions

--- prepare/Learning03/ClassFraction.cs
+++ prepare/Learning03/ClassFraction.cs
@@ -5,6 +5,8 @@ class Fraction
     private string TopNumber;
     private string BottomNumber;
 
+    // 3 forms, to allow the programmer to input 3 different amount of parameters (0 parameters, 1 parameter
+    // or 2 parameters).
     public void FractionFormat()
     {
         TopNumber = "1";
@@ -18,6 +20,28 @@ class Fraction
         TopNumber = Top;
         BottomNumber = Bottom;
     }
+
+
+    // Literally just reports 3/4
+    public string GetFractionString(){
+        TopNumber = "3";
+        BottomNumber = "4";
+
+        string FractionString = TopNumber + "/" + BottomNumber;
+        return FractionString;
+    }
+
+
+    // converts fractions to decimals
+    public int GetDecimalValue(){
+        int TopInt = Int32.Parse(TopNumber);
+        int BottomInt = Int32.Parse(BottomNumber);
+        int FractionDecimal = TopInt / BottomInt;
+        return FractionDecimal;
+    }
+
+
+    // my Setters
     public string SetTop(string Top){
         TopNumber = Top;
         return TopNumber;
@@ -25,5 +49,14 @@ class Fraction
     public string SetBottom(string Bottom){
         BottomNumber = Bottom;
         return BottomNumber;
+    }
+
+
+    // my Getters
+    public string GetTop(){
+        return TopNumber;
+    }
+    public string GetBottom(){
+        return TopNumber;
     }
 }
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -7,9 +7,26 @@ class Program
         Fraction Number1 = new Fraction();
         Number1.FractionFormat();
         Console.WriteLine(Number1);
+
+        Number1.GetDecimalValue();
+        Console.WriteLine(Number1);
+
         Number1.FractionFormat("5");
         Console.WriteLine(Number1);
+
+        Number1.GetDecimalValue();
+        Console.WriteLine(Number1);
+
         Number1.FractionFormat("5", "7");
+        Console.WriteLine(Number1);
+
+        Number1.GetDecimalValue();
+        Console.WriteLine(Number1);
+
+        Number1.GetFractionString();
+        Console.WriteLine(Number1);
+
+        Number1.GetDecimalValue();
         Console.WriteLine(Number1);
 
     }
