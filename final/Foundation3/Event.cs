986555c04bd8f1e4886ed55d9776b024b4cec006
Kaleb Bradford <wannabdiogenes@gmail.com>
2023-07-13 15:33:32 -0600

    Copied the Address Class directly to Foundation 3

--- final/Foundation3/Event.cs
+++ final/Foundation3/Event.cs
@@ -4,5 +4,42 @@ using System.Collections.Generic;
 
 class Event
 {
-    
+    string _title;
+    string _description;
+    string _date;
+    string _time;
+     _address;
+    string _eventType;
+
+    public Event(string title, string description, string date, string time, string address)
+    {
+        _title = title;
+        _description = description;
+        _date = date;
+        _time = time;
+        _address = address;
+    }
+    public Event()
+    {
+
+    }
+    private void EventType()
+    {
+        _eventType = "Generic";
+    }
+
+    protected string StandardDetails()
+    {
+        return $"{_title}, {_description}, {_date}, {_time}, {_address}";
+    }
+    protected string FullDetails()
+    {
+        // Needs to be created after all classes are completed
+        return "";
+    }
+    protected string ShortDescription()
+    {
+        // Needs to be created after all classes are completed
+        return "";
+    }
 }
