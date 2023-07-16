15bc88c1ade24e22ea28bc7caa4a4252996e6f3c
Kaleb Bradford <wannabdiogenes@gmail.com>
2023-07-16 16:31:57 -0600

    created a spinner and made console clear sometimes

--- prove/Develop04/Breathing.cs
+++ prove/Develop04/Breathing.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Linq;
 using System.Collections.Generic;
+using System.Threading;
 
 class Breathing : Activity
 {
@@ -11,6 +12,12 @@ class Breathing : Activity
     }
     public override void DefaultActivityDescription()
     {
-        _activityDescription = "This is an activity to focus on controlling breathing";
+        _activityDescription = @"This is an activity to focus on controlling breathing.
+In this activity, we will guide you through breathing in and out. Clear your mind, and focus on 
+breathing.";
+    }
+    public void BreathingActivity()
+    {
+        
     }
 }
